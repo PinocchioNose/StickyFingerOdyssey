b597e58bb4d0bc61d02f05f7a23560b04627bc7b
teeeeeedd <qiqirz@126.com>
2020-04-11 11:47:11 +0800

    preliminary complement of interaction interface

---
+++ Assets/Scripts/Interactive/handleIactRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handleIactRequest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool inputStatus = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inputStatus == true)
+            if(Input.GetKeyDown("e")){
+                this.gameObject.GetComponent<Iiact>().changeStat();
+                
+            }
+    }
+
+    //interface opened for objects require interaction with thisobj.
+    public void readyForIact(){
+        inputStatus = true;
+    }
+}
+
+
