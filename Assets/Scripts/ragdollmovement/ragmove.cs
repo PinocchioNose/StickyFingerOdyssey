83e5c76927dc7a42d166b840f724dda2ed7c7288
teeeeeedd <qiqirz@126.com>
2020-04-18 00:59:50 +0800

    initial active-ragdoll

---
+++ Assets/Scripts/ragdollmovement/ragmove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ragmove : MonoBehaviour
+{
+    public HingeJoint moveBone;
+    public Transform Obj;
+    public bool Invert;
+
+    // Update is called once per frame
+    void Update()
+    {
+        JointSpring moveForce = moveBone.spring;
+        
+        moveForce.targetPosition = Obj.transform.localEulerAngles.x;
+
+        if(moveForce.targetPosition > 180)
+            moveForce.targetPosition -= 360;
+
+        moveForce.targetPosition = Mathf.Clamp(moveForce.targetPosition,moveBone.limits.min + 5,moveBone.limits.max - 5);
+
+        if(Invert)
+            moveForce.targetPosition *= -1;
+
+        moveBone.spring = moveForce;      
+    }
+}
---
+++ Assets/pivot.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class pivot : MonoBehaviour
+{
+[MenuItem("Tool/ResetCenterPosition")]
+public static void ResetCenterPosition()
+{
+    Transform transform = Selection.activeTransform;
+    Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+    Vector3 center = Vector3.zero;
+    //获取物体的Bound最终合成信息
+    foreach (var item in transform.GetComponentsInChildren<MeshRenderer>())
+    {
+        bounds.Encapsulate(item.bounds);
+    }
+    center = bounds.center;
+    //新建空物体，将原来中心点有问题的物体放置到该物体下作为子物体存在
+    GameObject obj = new GameObject();
+    obj.name = transform.name;
+    obj.transform.position = center;
+    obj.transform.rotation = Quaternion.identity;
+    //获取原物体在模型中的路径
+    string selectedObjPath = "";
+    Transform currentSelectTransform = transform;
+    while (currentSelectTransform.parent != null)
+    {
+        selectedObjPath = currentSelectTransform.parent.name + "/" + selectedObjPath;
+        currentSelectTransform = currentSelectTransform.parent;
+    }
+    //设置空物体的层级，使之与原物体处于同一层级
+    obj.transform.SetParent(GameObject.Find(selectedObjPath).transform);
+    transform.SetParent(obj.transform);
+}
+}
