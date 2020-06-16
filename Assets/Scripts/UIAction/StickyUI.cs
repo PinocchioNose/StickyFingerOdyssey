9aaed2ca896826206fba0170d6ae631cefbce60d
TohsakaHaku <a853920798@outlook.com>
2020-06-16 17:58:47 +0800

    UI改动

--- Assets/Scripts/UIAction/StickyUI.cs
+++ Assets/Scripts/UIAction/StickyUI.cs
@@ -6,19 +6,31 @@ using UnityEngine.UI;
 public class StickyUI : MonoBehaviour
 {
     // 两个UI
-    public Image leftUI;
-    public Image rightUI;
+    public GameObject leftUI;
+    public GameObject rightUI;
+    public GameObject leftUI_after;
+    public GameObject rightUI_after;
+
+    private float lastTime;
     public void leftImageUIStatusChange(bool status)
     {
         Debug.Log("leftUI changed");
         if(status)
         {
             // 粘性的
-            leftUI.enabled = false;
+            leftUI.SetActive(true);
+            leftUI_after.SetActive(false);
+            //leftUI.enabled = true;
+            //leftUI_after.enabled = false;
+            lastTime = Time.time;
         }
         else
         {
-            leftUI.enabled = true;
+            leftUI.SetActive(false);
+            leftUI_after.SetActive(true);
+            //leftUI.enabled = false;
+            //leftUI_after.enabled = true;
+            lastTime = Time.time;
         }
     }
 
@@ -28,24 +40,51 @@ public class StickyUI : MonoBehaviour
         if (status)
         {
             // 粘性的
-            rightUI.enabled = false;
+            rightUI.SetActive(true);
+            rightUI_after.SetActive(false);
+            //rightUI.enabled = true;
+            //rightUI_after.enabled = false;
+            lastTime = Time.time;
         }
         else
         {
-            rightUI.enabled = true;
+            rightUI.SetActive(false);
+            rightUI_after.SetActive(true);
+            //rightUI.enabled = false;
+            //rightUI_after.enabled = true;
+            lastTime = Time.time;
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        leftUI.enabled = false;
-        rightUI.enabled = false;
+        //leftUI.enabled = false;
+        //rightUI.enabled = false;
+        //leftUI_after.enabled = false;
+        //rightUI_after.enabled = false;
+        leftUI.SetActive(false);
+        leftUI_after.SetActive(false);
+        rightUI.SetActive(false);
+        rightUI_after.SetActive(false);
+
+        lastTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time - lastTime > 3.0)
+        {
+            //Debug.Log("Disable UI");
+            //leftUI.enabled = false;
+            //rightUI.enabled = false;
+            //leftUI_after.enabled = false;
+            //rightUI_after.enabled = false;
+            leftUI.SetActive(false);
+            leftUI_after.SetActive(false);
+            rightUI.SetActive(false);
+            rightUI_after.SetActive(false);
+        }
     }
 }
