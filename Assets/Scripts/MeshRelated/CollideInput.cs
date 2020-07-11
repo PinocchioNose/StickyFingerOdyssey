7706e0d2f39797ecdb0e84d88405d9729358e967
teeeeeedd <qiqirz@126.com>
2020-07-11 13:16:52 +0800

    collide input fix

--- Assets/Scripts/MeshRelated/CollideInput.cs
+++ Assets/Scripts/MeshRelated/CollideInput.cs
@@ -5,35 +5,45 @@ using UnityEngine;
 public class CollideInput : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float TimeCount=1.0f;
+    public float TimeCount = 1.0f;
     public float forceOffset = 0.1f;
     public float force = 10f;
     private bool deformFlag=false;
     private Vector3 forcePoint;
     private float _time;
+    private bool no_recover = false;
+
     private MeshDeformer deformer;
 
     void Start()
     {
+        Debug.Log(TimeCount);
         _time = TimeCount;
+        if(TimeCount<0)
+            no_recover = true;
         deformer = this.gameObject.GetComponent<MeshDeformer>();
     }
     // Update is called once per frame
     void Update()
     {
         if(deformer){
-            if(_time>0 && deformFlag==true){
+            if(no_recover && deformFlag){
+                deformer.AddDeformingForce(forcePoint,force);
+            }
+            else if(_time>0 && deformFlag==true){
                 deformer.AddDeformingForce(forcePoint,force);
                 _time -= Time.deltaTime;
             }
-            if(_time<=0){
+            else if(_time<=0){
                 deformFlag = false;
                 _time = TimeCount;
             }
             
         }
-        else Debug.Log("deformer err");
-
+        else
+        {
+            Debug.Log("deformer err");
+        }
         // Debug.Log(test);
     }
 
